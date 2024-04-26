a874654dad61949f8365d94b4f7c959f8afbe30a
Dambar Sing <programmer.gshi18@gmail.com>
2024-04-26 15:09:19 +0545

    scale, repeat, stagger, yoyo in GSAP

--- script.js
+++ script.js
@@ -4,10 +4,19 @@ gsap.to(".box1",{
     delay:1,
     backgroundColor:"blue",
     rotate:360,
+    scale:0.5,
     borderRadius:"50%"
 })
 gsap.from(".box2",{
     x:1000,
     duration:2,
     delay:1
+})
+gsap.from("h1",{
+    opacity:0,
+    y:30,
+    duration:2,
+    delay:1,
+    stagger:0.5,
+    repeat:2
 })
