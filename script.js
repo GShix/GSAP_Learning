c99e6c0c0ff85ce46fdce8722672fc54069a0b2a
Dambar Sing <programmer.gshi18@gmail.com>
2024-04-29 11:15:23 +0545

    scrollTrigger: trigger,scroller,markers,start,end,scrub,pin

--- script.js
+++ script.js
@@ -18,3 +18,28 @@ tl.from(".main h1",{
     scale:0.5,
     duration:0.5
 })
+gsap.from(".main2 h2",{
+    scale:0,
+    delay:1,
+    duration:2,
+    scrollTrigger:{
+        trigger:".main2 h2",
+        scroller:"body",
+        // markers:true,
+        start:"top 60%",
+        end:"top 30%",
+        scrub:true
+    }
+})
+gsap.to(".main3 h1",{
+    transform:"translateX(-150%)",
+    scrollTrigger:{
+        trigger:".main3",
+        scroller:"body",
+        start:"top 0%",
+        end:"top -100%",
+        // markers:true,
+        scrub:2,
+        pin:true
+    },
+})
